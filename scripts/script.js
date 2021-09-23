fe5339ba97b438dfadd03bea170b74ec135bc1a0
Daniil Voshchuk <vochshukdaniil@gmail.com>
2021-09-23 15:32:03 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+const upBtn = document.querySelector('.slider__up-button')
+const downBtn = document.querySelector('.slider__down-button')
+
+const mainSlide = document.querySelector('.slider__images')
+const slidesCount = document.querySelectorAll('.slider__img-item').length
+
+let activeSlideIndex = 0
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+    
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+    }
+
+    const height = document.querySelector('body').clientHeight * 0.9
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+}
