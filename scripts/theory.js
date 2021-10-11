bc64b1e5eab213c975b30d094e1d9a411e1c1995
Daniil Voshchuk <vochshukdaniil@gmail.com>
2021-10-11 21:20:29 +0300

    Page theory done

---
+++ scripts/theory.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function () {
+	const text = document.querySelector('.theory__text')
+
+	for (let i = 0; i < text.querySelectorAll('img').length; i++) {
+		let w = text.querySelectorAll('img')[i].clientWidth
+		//let h = text.querySelectorAll('img')[i].clientHeight
+
+		text.querySelectorAll('img')[i].width = w * 0.5
+		//text.querySelectorAll('img')[i].height = h * 0.5
+	}
+
+	const button = document.querySelector('#dl_low')
+	const background = document.querySelector('#image-handler')
+
+	button.addEventListener('click', displayBlock)
+	background.addEventListener('click', displayNone)
+
+	function displayBlock() {
+		background.style.display = 'block'
+	}
+	function displayNone() {
+		background.style.display = 'none'
+	}
+})
