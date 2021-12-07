7c3b944afcdb0b42c48e43999f63b4ca50a2a592
Daniil Voshchuk <vochshukdaniil@gmail.com>
2021-12-07 11:26:06 +0300

    Preloader problem on structure page fixed, scrollbar modified, new VDA logo

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,13 +1,16 @@
-window.onload = async function () {
-	//await sleep(2000);
-	let preloader = document.querySelector('.preloader');
-	preloader.classList.add('preloader__hiding');
-	document.body.classList.remove('noscroll');
-	await sleep(500);
-	preloader.classList.add('preloader__none');
-	preloader.classList.remove('preloader__hiding');
-};
+{
+	document.body.onload = async function () {
+		if (window.location.pathname.split('/').pop() == 'structure.html') init();
+		await sleep(500);
+		let preloader = document.querySelector('.preloader');
+		preloader.classList.add('preloader__hiding');
+		document.body.classList.remove('noscroll');
+		await sleep(500);
+		preloader.classList.add('preloader__none');
+		preloader.classList.remove('preloader__hiding');
+	};
 
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
 }
